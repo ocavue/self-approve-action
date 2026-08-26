75a95cdc7e1b058ac2bd7acaf01389a8ee0b9e52
ocavue <ocavue@gmail.com>
2026-08-26 16:12:27 +1200

    fix lint errors

--- __tests__/codeowners.test.ts
+++ __tests__/codeowners.test.ts
@@ -32,9 +32,12 @@ describe('fetchCodeowners', () => {
     return {
       rest: {
         repos: {
-          getContent: async ({ path }: { path: string }) => {
-            if (path in results) return { data: results[path] }
-            throw Object.assign(new Error('Not Found'), { status: 404 })
+          getContent: ({ path }: { path: string }) => {
+            const content = results[path]
+            if (content != null) return Promise.resolve({ data: content })
+            return Promise.reject(
+              Object.assign(new Error('Not Found'), { status: 404 })
+            )
           }
         }
       }
@@ -59,9 +62,10 @@ describe('fetchCodeowners', () => {
     const octokit = {
       rest: {
         repos: {
-          getContent: async () => {
-            throw Object.assign(new Error('Server Error'), { status: 500 })
-          }
+          getContent: () =>
+            Promise.reject(
+              Object.assign(new Error('Server Error'), { status: 500 })
+            )
         }
       }
     } as unknown as Octokit
